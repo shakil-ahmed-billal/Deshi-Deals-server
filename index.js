74d9c501820ac6dbc854279c58c27a690d5bbc74
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-01-29 01:03:45 +0600

    mongoDB database connect and  t-start product post or get api make

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-const express = require("express");
 const cors = require("cors");
-const morgan = require('morgan');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev")); // Log requests using morgan
+
+const uri = process.env.DATA_BASE_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    // create a database collection
+    const db = client.db("Deshi-Deals");
+    const productsCollection = db.collection("products");
+
+    // brand product information api
+    app.get("/all-product", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/add-product", async (req, res) => {
+      const productsData = req.body;
+      const result = await productsCollection.insertOne(productsData);
+      res.send(result);
+    });
+    console.log("DB connect complete");
+  } finally {
+  }
+};
+run().catch(console.dir);
 
 // Start API endpoint
 app.get("/", (req, res) => {
